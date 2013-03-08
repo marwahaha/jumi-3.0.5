cf451d8354f209a2d4d2300ec469647dcb4ee49f
ghazal <roselavy@gmail.com>
2013-03-08 16:27:18 +0100

    hope last change to mod_jumi.php

--- admin/module/mod_jumi.php
+++ admin/module/mod_jumi.php
@@ -10,7 +10,13 @@ defined('_JEXEC') or die('Restricted access');
 if(!defined('DS')){
 	define('DS',DIRECTORY_SEPARATOR);
 }
-// Include the functions only once
+//
+if ($params->def('prepare_content', 1))
+{
+	JPluginHelper::importPlugin('content');
+	$module->content = JHtml::_('content.prepare', $module->content);
+}
+//
 require_once(dirname(__FILE__).DS.'helper.php');
 
 $code_written   = modJumiHelper::getCodeWritten($params); //code written or ""
