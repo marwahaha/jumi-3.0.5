7830ec9dfa77fd34f9ebd9879a5a6ac0d7e7a547
ghazal <ghazal@users.noreply.github.com>
2015-09-12 12:52:51 +0200

    Update default.php

--- admin/views/editapplication/tmpl/default.php
+++ admin/views/editapplication/tmpl/default.php
@@ -1,11 +1,9 @@
 <?php 
 /**
- * Joomla! 1.5 component sexy_polling
  *
- * @version $Id: default.php 2012-04-05 14:30:25 svn $
+ * @version $Id: default.php 2015-09-12  $
  * @author Simon Poghosyan
  * @package Joomla
- * @subpackage sexy_polling
  * @license GNU/GPL
  *
  *
